5c1aa021cb1f09c83f08bcb97c45e459a8cecaad
storm1er <le.storm1er@gmail.com>
2017-10-01 15:28:31 +0200

    replace webpack bundle name

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,9 +1,9 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/js/index.js',
+  entry: './src/js/webpack.js',
   output: {
-    filename: 'index.js',
+    filename: 'webpack.js',
     path: path.resolve(__dirname, '../dist/js')
   }
 };
